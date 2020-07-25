9c5c829e29bd2dc0c06ab267f64d40f09fa31fb8
Takafumi Sakakibara <at.famic@gmail.com>
2020-07-25 18:45:40 +0900

    update version 1.0.0 ta_svd

--- src/main.ts
+++ src/main.ts
@@ -15,9 +15,10 @@ import { PathUtils } from "./util/PathUtils";
  * 2. change twitter status to 'OFF' on 23:00 every day.
  * *please change the code when you want to change the timeschedule.
  */
+
 const cron = require('node-cron');
 // 1. change twitter status to 'ON' on 07:00 every day.
-cron.schedule('7 * * *', () => {
+cron.schedule('0 0 7 * * *', () => {
     console.log('change twitter status to \'ON\'');
     let twitterMain = new TwitterMain();
     let chatworkMain = new ChatworkMain();
@@ -28,7 +29,7 @@ cron.schedule('7 * * *', () => {
     })();
 });
 // 2. change twitter status to 'OFF' on 23:00 every day.
-cron.schedule('23 * * *', () => {
+cron.schedule('0 0 23 * * *', () => {
     console.log('change twitter status to \'OFF\'');
     let twitterMain = new TwitterMain();
     let chatworkMain = new ChatworkMain();
@@ -38,35 +39,3 @@ cron.schedule('23 * * *', () => {
         chatworkMain.process(twitterAdStatus);
     })();
 });
-
-
-
-
-
-// memo as follows:
-// let message = 'test message before updated';
-// let response;
-// (async () =>{
-//     response = await ChatworkUtils.sendMesages(new TwitterAdAutomation, message);
-//     console.log(response);
-//     message = 'updated message';
-//     response = await ChatworkUtils.updateMessages(new TwitterAdAutomation, message, response);
-// })();
-
-// let twitterMain: TwitterMain = new TwitterMain();
-// twitterMain.process();
-// let chatworkmain = new ChatworkMain();
-// chatworkmain.process(new TwitterAdStatus('OFF'));
-
-// TwitterMain.pdfMerge();
-
-// ChatworkUtils.sendFile(new TwitterAdAutomation, 'this is sending file test', 'twitter-login.png');
-
-// let twitterMain = new TwitterMain();
-// let chatworkMain = new ChatworkMain();
-// let twitterAdStatus = new TwitterAdStatus('ON');
-// (async () => {
-//     twitterMain.process(twitterAdStatus);
-//     chatworkMain.process(twitterAdStatus);
-// })();
-
